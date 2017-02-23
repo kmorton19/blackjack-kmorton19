95ffe3fde3859ffdae4d38860aa8404bc7933a2d
Kevin Morton <kmorton19@amdg.brebeuf.org>
2017-02-22 19:36:29 -0500

    Thank Ethan for helping me

---
+++ player-input.php
@@ -0,0 +1,6 @@
+<form id="board">
+    <input type="submit" value="deal" name="action">
+    <input type="submit" value="hit" name="action">
+    <input type="submit" value="pass" name="action">
+    <input type="submit" value="reset" name="action">
+    </form>
